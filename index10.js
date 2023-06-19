8c6c395794cd6561fb7433147f136cb115b84ee6
Анастасия <doby09@mail.ru>
2023-06-19 11:42:14 +0500

    release v1

---
+++ index10.js
@@ -0,0 +1,30 @@
+// Первое задание 
+
+function text() {
+    return ('Я учу JavaScript!');
+}
+console.log(text());
+
+//Второе задание
+
+const image = document.getElementById('img1');
+
+function myImage() {
+    image.src = 'images/cat2.jpg';
+}
+function returnImage() {
+    image.src = 'images/cat1.jpg';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
